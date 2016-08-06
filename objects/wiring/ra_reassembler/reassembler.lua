da88c3d25c6060ddeef1131245b5023185bdd021
kilkenni <cgustu@gmail.com>
2016-08-06 17:59:14 +0300

    Working renamer

--- objects/wiring/ra_reassembler/reassembler.lua
+++ objects/wiring/ra_reassembler/reassembler.lua
@@ -15,9 +15,18 @@ function selectHue(hue)
 end
 
 function ra.renameItem(msg, something, newName)
-	if newName == "" then
-		local default = root.itemConfig(object.name())
-		newName = default.config.shortdescription
+	if newName == "" or not world.containerItemAt(entity.id(), 0) then --if there is no new name or no gun
+		return false
 	end
+	if world.containerItemAt(entity.id(), 0).name ~= "commonassaultrifle" or world.containerItemAt(entity.id(), 2) then --if not assault rifle or slot 3 is occupied
+		return false
+	end
+
+	local item = world.containerTakeAt(entity.id(), 0) 
+	item.parameters.shortdescription = newName
+	for key,value in pairs(item) do
+		sb.logInfo("[HELP DUMP ]"..key.." : "..tostring(value));
+	end
+	world.containerPutItemsAt(entity.id(), item, 2)
 	object.setConfigParameter("shortdescription", newName)
 end

6fc913963cf1852707327d2261e91f2ed8582252
kilkenni <cgustu@gmail.com>
2016-08-08 08:56:23 +0300

    --With sounds!

--- interface/ra_reassembler/reassembler_gui.lua
+++ interface/ra_reassembler/reassembler_gui.lua
@@ -5,22 +5,69 @@ function init()
 	ra.renameVisible = false
 end
 
+function ra.isModGun()
+	if world.containerItemAt(pane.containerEntityId(), 0) then
+		return true
+	else
+		return false
+	end
+end
+
+function ra.isAssembledGun()
+	if world.containerItemAt(pane.containerEntityId(), 2) then
+		return true
+	else
+		return false
+	end
+end
+
+function ra.isTemplateGun()
+	if world.containerItemAt(pane.containerEntityId(), 1) then
+		return true
+	else
+		return false
+	end
+end
+
 function ra.testCallback(widgetName)
-	widget.playSound("/sfx/tools/pickaxe_precious.ogg")
+	widget.playSound("/sfx/interface/ship_confirm1.ogg")
 end
 
 function ra.renameButton(widgetName)
-  ra.renameVisible = not ra.renameVisible
-  widget.setVisible("ra_boxRename", ra.renameVisible)
-  widget.focus( ra.renameVisible and "ra_boxRename" or "ra_btnRename" )
+	if not ra.isModGun() or ra.isAssembledGun() then --if there is no gun or pick-up slot is occupied
+		widget.playSound("/sfx/interface/clickon_error.ogg")
+		return false
+	end
+	ra.renameVisible = not ra.renameVisible
+	widget.setVisible("ra_boxRename", ra.renameVisible)
+	widget.focus( ra.renameVisible and "ra_boxRename" or "ra_btnRename" )
 end
 
 function ra.reconstructButton(widgetName)
-	world.sendEntityMessage(pane.containerEntityId(), "reconstructGun")
+	if not ra.isModGun() or not ra.isTemplateGun() or ra.isAssembledGun() then --if there is no gun or no template gun or pick-up slot is occupied
+		widget.playSound("/sfx/interface/clickon_error.ogg")
+		return false
+	end
+	local result = world.sendEntityMessage(pane.containerEntityId(), "reconstructGun")
+	widget.playSound("/sfx/interface/penguin_welding4.ogg")
 end
 
 function ra.resetButton(widgetName)
+	if not ra.isModGun() or ra.isAssembledGun() then --if there is no gun or pick-up slot is occupied
+		widget.playSound("/sfx/interface/clickon_error.ogg")
+		return false
+	end
 	world.sendEntityMessage(pane.containerEntityId(), "resetGun")
+	widget.playSound("/sfx/interface/ship_confirm1.ogg")
+end
+
+function ra.scanButton(widgetName)
+	if not ra.isModGun() then --if there is no gun
+		widget.playSound("/sfx/interface/clickon_error.ogg")
+		return false
+	end
+	world.sendEntityMessage(pane.containerEntityId(), "scanGun")
+	widget.playSound("/sfx/objects/castleswitchon.ogg")
 end
 
 function ra.renameThis(widgetName)
--- objects/wiring/ra_reassembler/reassembler.lua
+++ objects/wiring/ra_reassembler/reassembler.lua
@@ -1,3 +1,6 @@
+--require "/scripts/staticrandom.lua"
+require "/scripts/util.lua"
+
 ra = {}
 
 function init()
@@ -6,14 +9,7 @@ function init()
 	message.setHandler("renameGun", ra.renameGun)
 	message.setHandler("reconstructGun", ra.reconstructGun)
 	message.setHandler("resetGun", ra.resetGun)
-end
-
-function selectHue(hue)
-	if world.containerItemAt(entity.id(), 0) and root.itemConfig(world.containerItemAt(entity.id(), 0)).config.materialId then
-		local item = world.containerTakeAt(entity.id(), 0) -- pick the item that was added
-		item.parameters.materialHueShift = hue
-		world.containerPutItemsAt(entity.id(), item, 0)
-	end
+	message.setHandler("scanGun", ra.scanGun)
 end
 
 function ra.renameGun(msg, something, newName)
@@ -30,24 +26,40 @@ function ra.renameGun(msg, something, newName)
 		sb.logInfo("[HELP DUMP ]"..key.." : "..tostring(value));
 	end
 	world.containerPutItemsAt(entity.id(), item, 2)
+	return true
 end
 
-function ra.resetGun(msg, something)
+function ra.scanGun(msg, something)
 	if not world.containerItemAt(entity.id(), 0) then --if there is no gun
 		return false
 	end
-	local modgun = root.itemConfig(world.containerItemAt(entity.id(), 0))
+	local modguncfg = root.itemConfig(world.containerItemAt(entity.id(), 0))
+	local modgun = world.containerItemAt(entity.id(), 0)
 	for key,value in pairs(modgun) do
+		sb.logInfo("[HELP DUMP gun]"..key.." : "..tostring(value))
+	end
+	for key,value in pairs(modguncfg.config) do
+		sb.logInfo("[HELP DUMP cfg.config]"..key.." : "..tostring(value))
+	end
+	return true
+end
+
+function ra.resetGun(msg, something)
+	if not world.containerItemAt(entity.id(), 0) or world.containerItemAt(entity.id(), 2) then --if there is no gun or pickup slot is occupied
+		return false
+	end
+	local modguncfg = root.itemConfig(world.containerItemAt(entity.id(), 0))
+	--[[for key,value in pairs(modguncfg) do
 		sb.logInfo("[HELP DUMP ]"..key.." : "..tostring(value))
 	end
-	if modgun.config then
+	if modguncfg.config then
 	sb.logInfo("[HELP DUMP ] CONFIG")
-		for key,value in pairs(modgun.config) do
+		for key,value in pairs(modguncfg.config) do
 			sb.logInfo("[HELP DUMP ]"..key.." : "..tostring(value))
 		end
-	end
-	local resetseed = modgun.parameters.seed
-	local resetlevel = modgun.parameters.level
+	end]]--
+	local resetseed = modguncfg.parameters.seed
+	local resetlevel = modguncfg.parameters.level
 	local resetgun_template = world.containerItemAt(entity.id(), 0)
 	--[[for key,value in pairs(resetgun_template) do
 		if key ~= "count" and key ~= "name" then
@@ -56,17 +68,19 @@ function ra.resetGun(msg, something)
 	end]]--
 	resetgun_template.parameters = nil
 	local resetgun = root.createItem(resetgun_template, resetlevel, resetseed)
-	for key,value in pairs(resetgun) do
+	--[[for key,value in pairs(resetgun) do
 		sb.logInfo("[HELP DUMP ] resetgun "..key.." : "..tostring(value))
-	end
+	end]]--
 	world.containerPutItemsAt(entity.id(), resetgun, 2)
+	world.containerTakeAt(entity.id(), 0)
+	return true
 	--[[if newName == "" or not world.containerItemAt(entity.id(), 0) then --if there is no new name or no gun
 		return false
 	end
 	if world.containerItemAt(entity.id(), 0).name ~= "commonassaultrifle" or world.containerItemAt(entity.id(), 2) then --if not assault rifle or slot 3 is occupied
 		return false
 	end
-	local item = world.containerTakeAt(entity.id(), 0) 
+	local item = world.containerTakeAt(entity.id(), 0)
 	item.parameters.shortdescription = newName
 	world.containerPutItemsAt(entity.id(), item, 2)]]--
 end
@@ -92,9 +106,11 @@ function ra.reconstructGun(msg, something, newName)
 	modgun.parameters.animationPartVariants = template.parameters.animationPartVariants
 	--modgun.parameters.paletteSwaps = templatecfg.config.paletteSwaps
 	--sb.logInfo("[HELP DUMP item]"..templatecfg.config.paletteSwaps);
-	for part,index in pairs(templatecfg.config) do
+	--[[for part,index in pairs(templatecfg.config.animationCustom) do
 		sb.logInfo("[HELP DUMP templatecfg]"..part.." : "..tostring(index));
-	end
+	end]]--
+	construct(modgun.parameters, "animationCustom", "sounds", "fire")
+	modgun.parameters.animationCustom.sounds.fire = templatecfg.config.animationCustom.sounds.fire
 	
 	world.containerPutItemsAt(entity.id(), modgun, 2)
 end
